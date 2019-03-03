763b7fb3f1ce271d56e3a82a2e2f5a8342dd9cf1
jaysonding <576938286@qq.com>
2019-03-03 23:05:31 +0800

    up

--- src/main/java/cn/iq99/util/ArrayUtil.java
+++ src/main/java/cn/iq99/util/ArrayUtil.java
@@ -1,15 +1,33 @@
 package cn.iq99.util;
 
 import lombok.Data;
-import lombok.extern.slf4j.Slf4j;
 
 @Data
-@Slf4j
 public class ArrayUtil {
 	
-	public static void printArray(int[] array){
-		for(int i=0;i<array.length;i++){
-			log.info(" {},",array[i]);
+	/**
+	 * 交换数组顺序
+	 * @param array
+	 * @param i
+	 * @param j
+	 * @return
+	 */
+	public static void swapArray(int[] array,int i,int j) {
+		
+		int temp=array[i];
+		array[i]=array[j];
+		array[j]=temp;
+	}
+	
+	/**
+	 * 打印数组
+	 * @param arr
+	 */
+	public static void printArray(int arr[]) {
+		
+		for(int i=0;i<arr.length;i++) {
+			System.out.print(arr[i]+" ");
 		}
+		System.out.println("");
 	}
 }
